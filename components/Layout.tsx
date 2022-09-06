1e212a57a19e9991d37859914876db34f686a87d
FimaLb <amifubar@gmail.com>
2022-09-06 20:47:00 +0300

    INTV-1 added favicon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -16,6 +16,7 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="icon" href="../static/favicon.ico" />
     </Head>
 
     <Header />

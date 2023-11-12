6015cbc974f76deb18e885e8c9653b86bcb1039e
Vladyslav Polishchuk <gm.vpolishchuk@gmail.com>
2023-11-12 18:27:56 +0100

    fix: build error for auth endpoint

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -1,9 +1,1 @@
-import NextAuth from 'next-auth';
-import Google from 'next-auth/providers/google';
-
-export const {
-  handlers: { GET, POST },
-  auth,
-} = NextAuth({
-  providers: [Google],
-});
+export { GET, POST } from '../../../../../auth.config';
